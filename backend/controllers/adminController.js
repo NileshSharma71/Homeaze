e8f08986cccaaf87f21af8caa0aec5c790084176
Nilesh Sharma <sharmanilesh7105@gmail.com>
2026-02-07 05:03:43 +0530

    making adminController for handling the api for adding worker

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,72 @@
+import validator from 'validator'
+import bcrypt from 'bcrypt'
+import {v2 as cloudinary} from 'cloudinary'
+import workerModel from '../models/workerModel.js'
+
+
+// api for adding worker
+const addWorker = async (req, res) => {
+
+    try {
+        
+        const { name, email, password, speciality, degree, experience, about, fees, address } = req.body
+        const imageFile = req.file;
+
+        if (!imageFile) {
+            return res.status(400).json({
+                success: false,
+                message: "Image file is required"
+            });
+        }
+
+        // checking for all data to add worker
+        if (!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address) {
+            return res.json({ success: false, message: "Missing Details" })
+        }
+
+        // validating email format
+        if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: "Please enter a valid email" })
+        }
+
+        // validating strong password
+        if (password.length < 8) {
+            return res.json({ success: false, message: "Please enter a strong password" })
+        }
+
+        // hashing user password
+        const salt = await bcrypt.genSalt(10); // the more no. round the more time it will take
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        // upload image to cloudinary
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: "image" });
+        const imageUrl = imageUpload.secure_url
+        
+        // save worker data to database
+        const workerData = {
+            name,
+            email,
+            password: hashedPassword,
+            speciality,
+            degree,
+            experience,
+            about,
+            fees,
+            address: typeof address === "string" ? JSON.parse(address) : address ,// convert address string to object
+            image: imageUrl,
+            date: Date.now()
+        }
+
+        // return success response with worker data
+        const newWorker = new workerModel(workerData)
+        await newWorker.save()
+
+        res.json({ success: true, message: "Worker added successfully"})
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+export {addWorker}
