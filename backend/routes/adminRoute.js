d9de1461bd30bbc5bdd29e70ffca8fecb20867c4
Prateek <prateek2504yadav@gmail.com>
2026-02-10 03:21:57 +0530

    making login route

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import { addWorker } from '../controllers/adminController.js'
+import { addWorker , loginAdmin} from '../controllers/adminController.js'
 import upload from '../middlewares/multer.js'
+import authAdmin from '../middlewares/authAdmin.js'
 
 const adminRouter = express.Router()
 
-adminRouter.post('/add-worker', upload.single('image'), addWorker)
+adminRouter.post('/add-worker',authAdmin, upload.single('image'), addWorker) //add-worker route
+
+adminRouter.post('/login',loginAdmin) //login route
 
 export default adminRouter
