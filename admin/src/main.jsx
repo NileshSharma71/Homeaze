ccd2b48c0f6f53890b2d53255f1c6339dbe05ea3
Nilesh Sharma <sharmanilesh7105@gmail.com>
2026-02-15 02:47:20 +0530

    some modification in app, main, tailwind.config.js

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,9 +2,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import AdminContextProvider from './context/AdminContext.jsx'
+import WorkerContextProvider from './context/workerContext.jsx'
+import AppContextProvider from './context/Appcontext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <AdminContextProvider>
+      <WorkerContextProvider>
+          <AppContextProvider>
+            <App />
+          </AppContextProvider>
+      </WorkerContextProvider>
+    </AdminContextProvider>
   </BrowserRouter>,
 )
--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary':"#006400", //darkgreen
+      }
+    },
   },
   plugins: [],
 }
