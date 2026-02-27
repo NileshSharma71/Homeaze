0fce81105a9b6f1e59f3661fa956dd1d375568d2
Prateek <prateek2504yadav@gmail.com>
2026-02-27 22:51:51 +0530

    complete the add worker page in admin panel

--- admin/src/context/Appcontext.jsx
+++ admin/src/context/Appcontext.jsx
@@ -3,9 +3,10 @@ import { createContext } from "react";
 export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
     
     const value = {
-
+        backendUrl,
     }
     return (
         <AppContext.Provider value={value}>
