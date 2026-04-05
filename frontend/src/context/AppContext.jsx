6d7ca8ccd512c2d518ee77806f7d4d3e0dfaba39
Prateek <prateek2504yadav@gmail.com>
2026-04-06 01:03:47 +0530

    making api to update user profile and integrate this to the frontend

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,54 +1,86 @@
 import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
-import axios from 'axios'
+import axios from "axios";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
+  const currencySymbol = "₹";
 
-    const currencySymbol = '₹' //use to include currency symbol throughout the app
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL //use to include backend url throughout the app
-    const [workers, setWorkers] = useState([])
+  const [workers, setWorkers] = useState([]);
 
-    const [token,setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : ""
+  );
 
-    // Getting workers using API
-    const getWorkersData = async () => {
+  const [userData, setUserData] = useState(false);
 
-        try {
+  // Getting workers using API
+  const getWorkersData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/worker/list");
 
-            const { data } = await axios.get(backendUrl + '/api/worker/list')
-            if (data.success) {
-                setWorkers(data.workers)
-            } else {
-                toast.error(data.message)
-            }
+      if (data.success) {
+        setWorkers(data.workers);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
 
-        } catch (error) {
-            console.log(error)
-            toast.error(error.message)
+  // Getting User Profile using API
+  const loadUserProfileData = async () => {
+    try {
+      const { data } = await axios.get(
+        backendUrl + "/api/user/get-profile",
+        {
+          headers: { token },
         }
+      );
 
+      if (data.success) {
+        setUserData(data.userData);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
     }
+  };
 
-    const value = {
-        workers,
-        currencySymbol,
-        backendUrl,
-        token, setToken,
-        backendUrl
+  useEffect(() => {
+    getWorkersData();
+  }, []);
+
+  useEffect(() => {
+    if (token) {
+      loadUserProfileData();
     }
+  }, [token]);
 
-    useEffect(() => {
-        getWorkersData()
-    }, [])
+  const value = {
+    workers,
+    getWorkersData,
+    currencySymbol,
+    backendUrl,
+    token,
+    setToken,
+    userData,
+    setUserData,
+    loadUserProfileData,
+  };
 
-    return (
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
+  return (
+    <AppContext.Provider value={value}>
+      {props.children}
+    </AppContext.Provider>
+  );
+};
 
-export default AppContextProvider
+export default AppContextProvider;
